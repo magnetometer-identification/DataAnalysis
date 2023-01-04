864b630363c0c32c4df57e5f8f90184a25109826
Mihael Trajbarič <26118837+mihaTrajbaric@users.noreply.github.com>
2023-01-04 22:30:30 +0100

    add windowed fourier dataset

--- notebooks/lib.py
+++ notebooks/lib.py
@@ -19,10 +19,6 @@ def read_dataset(data_path, classes = 2):
             print(i,test_case)
         data_off = {test_case: pd.read_csv(glob.glob(f"{data_path}/*stage_5*{test_case}*")[0]) for test_case in test_cases}
         data_on = {test_case: pd.read_csv(glob.glob(f"{data_path}/*stage_6*{test_case}*")[0]) for test_case in test_cases}
-        # for key in data_off.keys():
-        #     add_intensity(data_off[key])
-        # for key in data_on.keys():
-        #     add_intensity(data_on[key])
         return data_on, data_off
     else:
         raise Exception("cannot process 6 classes")
